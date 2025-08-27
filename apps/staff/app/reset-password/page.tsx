944384ce97c95ecb22b98c165166990f8a299da0
Kimeshan Naidoo <kimeshan@gmail.com>
2025-08-27 04:43:36 +0300

    fix build

--- apps/staff/app/reset-password/page.tsx
+++ apps/staff/app/reset-password/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import { ResetPasswordPage } from '../../components/reset-password-page';
 
 export default function ResetPassword() {
-  return <ResetPasswordPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordPage />
+    </Suspense>
+  );
 }
 
 export const metadata = {
